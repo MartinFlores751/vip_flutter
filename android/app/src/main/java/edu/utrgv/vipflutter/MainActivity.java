bc32fae77f2d612a00697baf05e1d90a0e20183c
Martin Jaime Flores Jr <mf34314@gmail.com>
2019-02-14 13:22:14 -0600

    Added Copyright stuff for practice's sake.

--- android/app/src/main/java/edu/utrgv/vipflutter/MainActivity.java
+++ android/app/src/main/java/edu/utrgv/vipflutter/MainActivity.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2019. The AUTHORS - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ */
+
 package edu.utrgv.vipflutter;
 
 import android.os.Bundle;
