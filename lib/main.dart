2b57e2c440dbd34b1db995d34103c945a47573ad
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2019-05-29 03:41:37 -0500

    Refactor main, relocate logged

--- lib/main.dart
+++ lib/main.dart
@@ -4,329 +4,194 @@
  *  Proprietary and confidential
  */
 
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:flutter_udid/flutter_udid.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:vip_flutter/sign_up.dart';
-import 'package:vip_flutter/logged_acc.dart';
-import 'package:vip_flutter/logged_accVIP.dart';
+import 'package:vip_flutter/go_login.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class GoLogin extends StatefulWidget {
-  String username;
-  String password;
-  GoLogin(this.username, this.password);
-  @override
-  _GoLoginState createState() => _GoLoginState(username, password);
-}
-
-class _GoLoginState extends State<GoLogin> {
-  String username;
-  String password;
-  _GoLoginState(this.username, this.password);
-  String udid;
-  String token;
-  String urlBase = "https://vip-serv.herokuapp.com/api";
-
-  Future<dynamic> connect() async{
-    udid = await FlutterUdid.consistentUdid;
-    Map<String, String> body = {
-      'user': username,
-      'password': password,
-      'UUID': udid,
-    };
-    var url = urlBase + "/authenticate_user";
-    var response = await http.post(url, body: body);
-    var jsonResp = jsonDecode(response.body);
-    return jsonResp;
-  }
-  Future<dynamic> getHelpers() async{
-    Map<String, String> body = {
-      "token": token,
-      "UUID": udid,
-    };
-    var url = urlBase + "/get_helpers";
-    var response = await http.post(url, body: body);
-    return jsonDecode(response.body);
-  }
-  Future<dynamic> getVips() async{
-    Map<String, String> body = {
-      "token": token,
-      "UUID": udid,
-    };
-    var url = urlBase + "/get_VIP";
-    var response = await http.post(url, body: body);
-    return jsonDecode(response.body);
-  }
-  Widget theBuilder(){
-    return FutureBuilder<dynamic>(
-      future: connect(), // a previously-obtained Future<String> or null
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return CircularProgressIndicator();
-          case ConnectionState.active:
-            return CircularProgressIndicator();
-          case ConnectionState.waiting:
-            return CircularProgressIndicator();
-          case ConnectionState.done:
-            if (snapshot.hasError)
-              return Text('Error: ${snapshot.error}');
-            var response = snapshot.data;
-            print("PASS CONNECT: $response");
-            if (!response["success"]){          //THIS THING HAS AN ERROR
-                print("ERROR AFTER THIS BOIS");
-                Fluttertoast.showToast(msg: '${response["error"]}',toastLength: Toast.LENGTH_SHORT);
-                print("alsdjfsd");
-                Navigator.pop(context);
-                print("WADDUP");
-                return Container();
-            }
-            if (response["success"]){
-              bool helper = response["isHelper"];
-              token = response["token"];
-              print("GOING IN BOIS");
-              return FutureBuilder<dynamic>(
-                  future: helper ? getVips() : getHelpers(), // a previously-obtained Future<String> or null
-                  builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.none:
-                        return CircularProgressIndicator();
-                      case ConnectionState.active:
-                        return CircularProgressIndicator();
-                      case ConnectionState.waiting:
-                        return CircularProgressIndicator();
-                      case ConnectionState.done:
-                        if (snapshot.hasError)
-                          return Text('Error: ${snapshot.error}');
-                        var response2 = snapshot.data;
-                        if (response2["success"]){
-                          List<dynamic> allusers = jsonDecode(response2["users"]);
-                          Fluttertoast.showToast(msg: 'Login Successful',toastLength: Toast.LENGTH_SHORT);
-                          if (helper == false){
-                            final DocumentReference postRef = Firestore.instance.collection("Users").document('OnlineCount');
-                            Firestore.instance.runTransaction((Transaction tx) async {
-                              DocumentSnapshot postSnapshot = await tx.get(postRef);
-                              if (postSnapshot.exists) {
-                                await tx.update(postRef, <String, dynamic>{'TotalOnline': postSnapshot.data['TotalOnline'] + 1});
-                                await tx.update(postRef, <String, dynamic>{'VipOnline': postSnapshot.data['VipOnline'] + 1});
-                              }
-                              Map<String, dynamic> body = {
-                                  "${username}":{
-                                  "away": false,
-                                  "online": true,
-                                }
-                              };
-                              Firestore.instance.collection('Users').document('allUsers').updateData(body);
-                              Firestore.instance.collection('Users').document('allVip').updateData(body); //Change to allVips
-                            });
-                            return loggedAccVIP(token, allusers, username);
-                          }
-                          else
-                          {
-                            final DocumentReference postRef = Firestore.instance.collection("Users").document('OnlineCount');
-                            Firestore.instance.runTransaction((Transaction tx) async {
-                              DocumentSnapshot postSnapshot = await tx.get(postRef);
-                              if (postSnapshot.exists) {
-                                await tx.update(postRef, <String, dynamic>{'TotalOnline': postSnapshot.data['TotalOnline'] + 1});
-                                await tx.update(postRef, <String, dynamic>{'HelpersOnline': postSnapshot.data['HelpersOnline'] + 1});
-                              }
-                              Map<String, dynamic> body = {
-                                "${username}":{
-                                  "away": false,
-                                  "online": true,
-                                }
-                              };
-                              Firestore.instance.collection('Users').document('allUsers').updateData(body);
-                              Firestore.instance.collection('Users').document('allHelpers').updateData(body);
-                            });
-                            return loggedAcc(token); //replace (token) with (token, allusers, _username.text)
-                          }
-                        }
-                    }
-                    return null; // unreachable
-                  },
-                );
-            }
-        }
-        return null; // unreachable
-      },
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //appBar: AppBar(title: Text("")),
-      body: Align(
-        alignment: FractionalOffset(.5, .5),
-        child: theBuilder(),
-      ),
-    );
+        title: 'VIP Flutter',
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        initialRoute: MyHomePage.routeName,
+        routes: {
+          MyHomePage.routeName: (context) => MyHomePage(),
+        });
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
+  static const routeName = '/';
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
   var _username = TextEditingController();
   var _password = TextEditingController();
   String urlBase = "https://vip-serv.herokuapp.com/api";
-  
-  @override
-  Widget build(BuildContext context) {
-    FocusNode textSecondFocusNode = new FocusNode();
 
-    var loginCredentials = new Column(children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-          width: MediaQuery.of(context).size.width/1.5,
-          child: TextFormField(
-            inputFormatters: [
-              new WhitelistingTextInputFormatter(new RegExp("[a-zA-Z0-9]")),
-            ],
-            onFieldSubmitted: (String value) {
-              FocusScope.of(context).requestFocus(textSecondFocusNode);
-            },
-            controller: _username,
-            autofocus: false,
-            obscureText: false,
-            textInputAction: TextInputAction.next,
-            decoration: new InputDecoration(
-              border: new OutlineInputBorder(borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: new BorderSide(color: Colors.teal)),
-              hintText: 'Username',
-              labelText: 'Username',
-              prefixIcon: const Icon(
-                Icons.person,
-                color: Colors.blue,
-              ),
-              prefixText: ' ',
-            ),
+  Widget get _usernameField {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      width: MediaQuery.of(context).size.width / 1.5,
+      child: TextFormField(
+        inputFormatters: [
+          new WhitelistingTextInputFormatter(new RegExp("[a-zA-Z0-9]")),
+        ],
+        controller: _username,
+        autofocus: false,
+        obscureText: false,
+        textInputAction: TextInputAction.next,
+        decoration: new InputDecoration(
+          border: new OutlineInputBorder(
+              borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+              borderSide: new BorderSide(color: Colors.teal)),
+          hintText: 'Username',
+          labelText: 'Username',
+          prefixIcon: const Icon(
+            Icons.person,
+            color: Colors.blue,
           ),
+          prefixText: ' ',
         ),
-        SizedBox(height: 10,),
-        Container(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-          width: MediaQuery.of(context).size.width/1.5,
-          child: TextFormField(
-            inputFormatters: [
-              new BlacklistingTextInputFormatter(new RegExp("[ ]")),
-            ],
-            focusNode: textSecondFocusNode,
-            controller: _password,
-            autofocus: false,
-            obscureText: true,
-            decoration: new InputDecoration(
-              border: new OutlineInputBorder(borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: new BorderSide(color: Colors.teal)),
-              hintText: '******',
-              labelText: 'Password',
-              prefixIcon: const Icon(
-                Icons.enhanced_encryption,
-                color: Colors.blue,
-              ),
-              suffixStyle: const TextStyle(color: Colors.green)
-            ),
-          ),
-        ),
-      ],
+        validator: (value) {
+          if(value.length == 0)
+            return 'Please input a valid username!';
+        },
+      ),
     );
+  }
 
-    var loggedAccScreen2 = RaisedButton(
+  Widget get _passwordField {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      width: MediaQuery.of(context).size.width / 1.5,
+      child: TextFormField(
+        inputFormatters: [
+          new BlacklistingTextInputFormatter(new RegExp("[ ]")),
+        ],
+        controller: _password,
+        obscureText: true,
+        decoration: new InputDecoration(
+            border: new OutlineInputBorder(
+                borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                borderSide: new BorderSide(color: Colors.teal)),
+            hintText: '******',
+            labelText: 'Password',
+            prefixIcon: const Icon(
+              Icons.enhanced_encryption,
+              color: Colors.blue,
+            ),
+            suffixStyle: const TextStyle(color: Colors.green)),
+        validator: (value) {
+          if (value.length == 0)
+            return 'Please input a valid password!';
+        },
+      ),
+    );
+  }
+
+  Widget get _loginButton {
+    return RaisedButton(
       elevation: 2,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Text("Login"),
       color: Colors.blue[200],
-      onPressed: (){
-        if (_username.text.length > 0 && _password.text.length > 0){
+      onPressed: _validateForm,
+    );
+  }
+
+  void _validateForm() {
+    if (_formKey.currentState.validate()) {
+      // Do FireBase connection stuff here!
+      Navigator.of(context).push(
+        MaterialPageRoute(
+            builder: (context) => GoLogin(username: _username.text, password: _password.text)),
+      );
+    } else {
+      Fluttertoast.showToast(
+          msg: 'Field(s) Empty', toastLength: Toast.LENGTH_SHORT);
+    }
+  }
+
+  Widget get _newUserText {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text('New User?'),
+      FlatButton(
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.blue),
+        ),
+        onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) =>  GoLogin(_username.text, _password.text)),
+            MaterialPageRoute(builder: (context) => signUp(urlBase)),
           );
-        }
-        else{
-          Fluttertoast.showToast(msg: 'Field(s) Empty',toastLength: Toast.LENGTH_SHORT);
-        }
-      },
-    );
+        },
+      )
+    ]);
+  }
 
-    var createNewUser = new Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('New User?'),
-        FlatButton(
-          child: Text(
-            'Sign Up',
-            style: TextStyle(color: Colors.blue),
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => signUp(urlBase)),
-            );
-          },
-        )
-      ]
-    );
-    var theIcon = Icon(
+  Widget get _logoIcon {
+    return Icon(
       Icons.camera,
       size: 90,
-      //color: Colors.blue[800],
     );
-    var theContainer = Container(
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        shape: BoxShape.circle,
-        border: Border.all(width: 5, color: Colors.black)
-        
-      ),
-      child: theIcon,
-    );
+  }
 
-    return Scaffold(
-      //resizeToAvoidBottomPadding: false,
-      body: new Align(
-        alignment: FractionalOffset(.5, .5),
-        child: Container(
-          //padding: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height/5, 0, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[theContainer, SizedBox(height: 15), loginCredentials, loggedAccScreen2, createNewUser],
+  Widget get _logoBox {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          shape: BoxShape.circle,
+          border: Border.all(width: 5, color: Colors.black)),
+      child: _logoIcon,
+    );
+  }
+
+  Widget get _loginForm {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _logoBox,
+          SizedBox(height: 15),
+          Column(
+            children: <Widget>[
+              _usernameField,
+              SizedBox(
+                height: 10,
+              ),
+              _passwordField
+            ],
           ),
-        ),
+          _loginButton,
+          _newUserText
+        ],
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _loginForm),
     );
   }
 }
